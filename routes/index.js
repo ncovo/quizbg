36e5287ae9d50b5702b131287b03d354f3d09a1b
Bruno Gomez <bgomez@ncovo.com>
2015-06-06 17:49:28 +0200

    Página de autor

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,12 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/creditos', function(req, res){
+  res.render('creditos', { title: 'Quiz' });
+});
+
+
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
