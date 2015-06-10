210eff7871e6fb141ec091ea8b8dfa18357536de
Bruno Gomez <bgomez@ncovo.com>
2015-06-10 20:14:11 +0200

    Despliegue en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,12 +1,19 @@
+var models = require('../models/models.js');
 
+//GET /quizes/question
 exports.question = function(req, res){
-    res.render('quizes/question', {question: 'Capital de Italia', title: 'Quiz'});
+    models.Quiz.findAll().then(function(Quiz){
+	res.render('quizes/question', {question: Quiz[0].pregunta})
+    })
 };
 
+//GET /quizes/answer
 exports.answer = function(req, res){
-    if(req.query.answer === "Roma"){
-        res.render('quizes/answer', {answer: 'Correcto!!', title: 'Quiz'});
-    } else {
-        res.render('quizes/answer', {answer: 'Incorrecto!!', title: 'Quiz'});
-    }
+    models.Quiz.findAll().then(function(Quiz){
+        if(req.query.answer === "Roma"){
+            res.render('quizes/answer', {answer: 'Correcto!!', title: 'Quiz'});
+        } else {
+            res.render('quizes/answer', {answer: 'Incorrecto!!', title: 'Quiz'});
+        }
+    })
 };
