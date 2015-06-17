6b0ba8c6dc9110f0ac800dc21797d8110ba375c8
Bruno Gomez <bgomez@ncovo.com>
2015-06-17 13:48:32 +0200

    Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -2,6 +2,7 @@ var path = require('path');
 
 //Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 //SQLite   DATABASE_URL = slite://:@:/
+
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name  = (url[6]|null);
 var user     = (url[2]|null);
@@ -12,15 +13,15 @@ var port     = (url[5]|null);
 var host     = (url[4]|null);
 var storage  = process.env.DATABASE_STORAGE;
 
-console.log(user);
+console.log(url[1]);
 
 //cargar model ORM
 var Sequelize = require('sequelize');
 
 //Usar BBDD SQLite o Postgres
 var sequelize = new Sequelize(DB_name, user, pwd,
-    { dialect:   dialect,
-      protocol:  protocol,
+    { dialect:   url[1],
+      protocol:  url[1],
       port:      port,
       host:      host,
       storage:   storage, //solo SQLite (.env)
