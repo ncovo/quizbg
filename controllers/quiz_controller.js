7a721931795cf53a2cdc314169ac088093ed7ee7
Bruno Gomez <bgomez@ncovo.com>
2015-06-17 17:58:46 +0200

    Cambios heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,12 +1,26 @@
+var models = require('../models/models.js');
 
-exports.question = function(req, res){
-    res.render('quizes/question', {question: 'Capital de Italia', title: 'Quiz'});
+//GET /quizes
+exports.index = function(req, res){
+    models.Quiz.findAll().then(function(Quizes){
+	res.render('quizes/index.ejs', {Quizes: Quizes})
+    })
 };
 
+//GET /quizes/show
+exports.show = function(req, res){
+    models.Quiz.findById(req.params.quizId).then(function(Quiz){
+	res.render('quizes/show', {question: Quiz})
+    })
+};
+
+//GET /quizes/answer
 exports.answer = function(req, res){
-    if(req.query.answer === "Roma"){
-        res.render('quizes/answer', {answer: 'Correcto!!', title: 'Quiz'});
-    } else {
-        res.render('quizes/answer', {answer: 'Incorrecto!!', title: 'Quiz'});
-    }
+    models.Quiz.findById(req.params.quizId).then(function(Quiz){
+        if(req.query.answer === Quiz.respuesta){
+            res.render('quizes/answer', {question: Quiz, answer: 'Correcto!!', title: 'Quiz'});
+        } else {
+            res.render('quizes/answer', {question: Quiz, answer: 'Incorrecto!!', title: 'Quiz'});
+        }
+    })
 };
