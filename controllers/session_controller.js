baad1819937de7a060c9c2b255e20cf673f8a41d
Bruno Gomez <bgomez@ncovo.com>
2015-08-15 11:42:36 +0200

    autorizacion

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,4 +1,12 @@
 //GET login -- Formulario de login
+exports.loginRequired = function(req, res, next){
+	if (req.session.user){
+		next();
+	} else {
+		res.redirect('/login');
+	}
+};
+
 exports.new = function(req.res){
 	var errors = req.session.errors || {};
 	req.session.errors = {};
